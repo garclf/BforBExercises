d41a6918c34c25cff607d0b2dc2fea19a8d0157b
Felipe <garclf@live.com>
2018-12-09 19:32:39 -0800

    Added missing piece
    added handler in case there is no argument passed

--- src/com/ex1/Main/Main.java
+++ src/com/ex1/Main/Main.java
@@ -12,38 +12,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Path getDir = Paths.get(args[0]);
+        if(args.length > 0) {
+            Path getDir = Paths.get(args[0]);
+            if (Files.exists(getDir) && Files.isDirectory(getDir)) {
+                ArrayList<File> saveNames = new ArrayList<>();
 
+                FileDirHandler fileDirHandler = FileDirHandler.getInstance();
+                File[] filesInDir = fileDirHandler.getDirContents(args[0]);
 
-        if(Files.exists(getDir) && Files.isDirectory(getDir)) {
-            ArrayList<File> saveNames = new ArrayList<>();
+                for (File file : filesInDir) {
+                    if (file.isDirectory()) {
+                        File[] subdirFiles = fileDirHandler.getDirContents(file.getAbsolutePath());
 
-            FileDirHandler fileDirHandler = FileDirHandler.getInstance();
-            File[] filesInDir = fileDirHandler.getDirContents(args[0]);
-
-            for(File file: filesInDir) {
-                if(file.isDirectory()) {
-                    File[] subdirFiles = fileDirHandler.getDirContents(file.getAbsolutePath());
-
-                    for(File sFile: subdirFiles) {
-                        saveNames.add(sFile);
+                        for (File sFile : subdirFiles) {
+                            saveNames.add(sFile);
+                        }
+                    } else {
+                        saveNames.add(file);
                     }
-                } else {
-                    saveNames.add(file);
                 }
+
+                System.out.printf("%s\t\t\t\t%s\t\t\t\t%s\t\t\t\t\n", "Full Path", "Name", "Size");
+
+                fileDirHandler.sortAndDisplay(saveNames);
+
+            } else if (Files.isRegularFile(getDir)) {
+                System.out.println("The path you provided " + getDir.toString() + " is a file");
+            } else {
+                System.out.println("The path to the directory you provided does not exist... \n" +
+                        "Please provide an existent directory name");
             }
-
-            System.out.printf("%s\t\t\t\t%s\t\t\t\t%s\t\t\t\t\n", "Full Path", "Name", "Size");
-
-            fileDirHandler.sortAndDisplay(saveNames);
-
-        } else if (Files.isRegularFile(getDir)) {
-            System.out.println("The path you provided " + getDir.toString() + " is a file");
         } else {
-            System.out.println("The path to the directory you provided does not exist... \n" +
-                    "Please provide an existent directory name");
+            System.out.println("Please provide a path to a directory to list its files");
         }
-
 
     }
 }
